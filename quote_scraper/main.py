f0c33f0c69a17a8ce4386161840f9eb9490507f7
Aleksander Khreptak <alex.nuclearboy@gmail.com>
2025-02-18 13:14:54 +0100

    Move the new folder creation to the writing module

--- quote_scraper/main.py
+++ quote_scraper/main.py
@@ -3,7 +3,6 @@ from scraper.scraper import scrape_all_data
 from storage.json_writer import write_to_json
 
 BASE_URL = 'https://quotes.toscrape.com'
-OUTPUT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'data')
 
 def ensure_directory_exists(directory):
     if not os.path.exists(directory):
@@ -12,12 +11,9 @@ def ensure_directory_exists(directory):
 def main():
     quotes, authors = scrape_all_data(BASE_URL)
 
-    # Ensure the output directory exists
-    ensure_directory_exists(OUTPUT_DIR)
-
     # Save data to JSON files in the output directory
-    write_to_json(quotes, os.path.join(OUTPUT_DIR, 'quotes.json'))
-    write_to_json(authors, os.path.join(OUTPUT_DIR, 'authors.json'))
+    write_to_json(quotes, 'quotes.json')
+    write_to_json(authors, 'authors.json')
 
 if __name__ == '__main__':
     main()
--- quote_scraper/storage/json_writer.py
+++ quote_scraper/storage/json_writer.py
@@ -1,11 +1,23 @@
+import os
 import json
+
+# Define the path to the 'data' folder
+OUTPUT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../..', 'data')
+
+def ensure_directory_exists(directory):
+    """Ensure the 'data' folder exists, create it if it doesn't"""
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
 def write_to_json(data, filename):
     """
     Write data to a JSON file.
     """
+    ensure_directory_exists(OUTPUT_DIR)
+
     try:
-        with open(filename, 'w', encoding='utf-8') as f:
+        file_path = os.path.join(OUTPUT_DIR, filename)
+        with open(file_path, 'w', encoding='utf-8') as f:
             json.dump(data, f, ensure_ascii=False, indent=2)
         print(f"Data successfully written to {filename}")
     except Exception as e:

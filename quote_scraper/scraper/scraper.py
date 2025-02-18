1187846097334389fba30886be02a1310c5b09b4
Aleksander Khreptak <alex.nuclearboy@gmail.com>
2025-02-18 14:56:52 +0100

    Format the code and add a page scraper

--- quote_scraper/scraper/scraper.py
+++ quote_scraper/scraper/scraper.py
@@ -3,32 +3,33 @@ from bs4 import BeautifulSoup
 
 from utils.date_processor import parse_date
 from utils.text_processor import extract_text
-from utils.url_processor import extract_url
-
-BASE_URL = 'https://quotes.toscrape.com'
+from utils.url_processor import extract_url, get_next_page_url
 
 
-def fetch_quotes(soup, authors_urls):
+def fetch_quotes(soup, base_url):
     quotes = []
 
     for quote in soup.find_all('div', class_='quote'):
-        text = extract_text(quote.find('span', class_='text'))
-        author = extract_text(quote.find('small', class_='author'))
+        text = extract_text(
+            quote.find('span', class_='text'), 'No text available'
+        )
+        author = extract_text(
+            quote.find('small', class_='author'), 'Unknown Author'
+        )
         author_link_tag = quote.find('a')
         if author_link_tag:
             author_url = extract_url(
-                author_link_tag, 'href', base_url=BASE_URL
+                author_link_tag, 'href', base_url=base_url
             )
         tags = [
                 extract_text(tag)
                 for tag in quote.find_all('a', class_='tag')
                 if tag.text.strip()  # Filter out empty tags
             ]
-        if author not in authors_urls:
-            authors_urls[author] = author_url
         quotes.append({
             'text': text,
             'author': author,
+            'author_url': author_url,
             'tags': tags
         })
 
@@ -38,42 +39,62 @@ def fetch_quotes(soup, authors_urls):
 def fetch_author_details(author_url):
     response = requests.get(author_url)
     soup = BeautifulSoup(response.text, 'lxml')
-    fullname = extract_text(soup.find('h3', class_='author-title'))
+    fullname = extract_text(
+        soup.find('h3', class_='author-title'), 'Unknown Author'
+    )
     birth_date = parse_date(
                 extract_text(
                     soup.find('span', class_='author-born-date')
                 )
             )
-    birth_location = extract_text(soup.find('span', class_='author-born-location'))
-    description = extract_text(soup.find('div', class_='author-description'))
+    birth_location = extract_text(
+        soup.find('span', class_='author-born-location'), 'Unknown Location'
+    )
+    bio = extract_text(
+        soup.find('div', class_='author-description'), 'No bio available'
+    )
 
     return {
         "fullname": fullname,
         "birth_date": birth_date,
         "birth_location": birth_location,
-        "description": description
+        "bio": bio
     }
 
 
-def scrape_all_data(initial_url):
-    quotes = []
-    authors_urls = {}
-    url = initial_url
+def scrape_page_data(base_url, page_url, fetched_authors):
+    response = requests.get(page_url)
+    soup = BeautifulSoup(response.text, 'lxml')
 
-    while url:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'lxml')
-        quotes += fetch_quotes(soup, authors_urls)
+    quotes = fetch_quotes(soup, base_url)
 
-        next_btn = soup.find('li', class_='next')
-        if next_btn:
-            url = initial_url + next_btn.find('a')['href']
-        else:
-            url = None
+    authors_urls = {
+        quote['author_url'] for quote in quotes if quote['author_url']
+        and quote['author_url'] not in fetched_authors
+    }
 
-    authors_details = [
-        fetch_author_details(url) for url in authors_urls.values()
-    ]
+    authors_details = [fetch_author_details(url) for url in authors_urls]
+
+    fetched_authors.update(authors_urls)
 
     return quotes, authors_details
 
+
+def scrape_all_data(base_url):
+    all_quotes = []
+    all_authors_details = []
+    fetched_authors = set()
+    current_url = base_url
+
+    while current_url:
+        quotes, authors_details = scrape_page_data(base_url, current_url,
+                                                   fetched_authors)
+        all_quotes.extend(quotes)
+        all_authors_details.extend(authors_details)
+
+        next_url = get_next_page_url(
+            current_url, base_url, ['li.next a'], 'href'
+        )
+        current_url = next_url
+
+    return all_quotes, all_authors_details
--- quote_scraper/utils/url_processor.py
+++ quote_scraper/utils/url_processor.py
@@ -1,7 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse
 
 
-def extract_url(tag, attribute = 'href', default = None, base_url = None):
+def extract_url(tag, attribute='href', default=None, base_url=None):
     if not tag or not tag.has_attr(attribute):
         return default
 
@@ -22,3 +24,30 @@ def is_valid_url(url):
 
     parsed = urlparse(url)
     return bool(parsed.scheme and parsed.netloc)
+
+
+def get_next_page_url(current_url, base_url, selectors=None, attribute='href'):
+    if selectors is None:
+        selectors = ['li.next a', 'a.next_page']
+
+    response = requests.get(current_url)
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    # Try fetching the next URL from the selectors
+    next_url = find_next_url(soup, selectors, current_url, base_url, attribute)
+    if next_url:
+        return next_url
+
+    return None
+
+
+def find_next_url(soup, selectors, current_url, base_url, attribute):
+    for selector in selectors:
+        next_element = soup.select_one(selector)
+        if next_element:
+            next_url = extract_url(
+                next_element, attribute, default=None, base_url=base_url
+            )
+            if next_url and next_url != current_url:
+                return next_url
+    return None

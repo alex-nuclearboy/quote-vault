e6b1757b7d1bb6598cd3b9510ad36faaee68227f
Aleksander Khreptak <alex.nuclearboy@gmail.com>
2025-02-24 11:51:30 +0100

    Add a logging module

--- quote_scraper/scraper/scraper.py
+++ quote_scraper/scraper/scraper.py
@@ -4,9 +4,13 @@ from bs4 import BeautifulSoup
 from utils.date_processor import parse_date
 from utils.text_processor import extract_text
 from utils.url_processor import extract_url, get_next_page_url
+from utils.logger import setup_logger
+
+logger = setup_logger(__name__)
 
 
 def fetch_quotes(soup, base_url):
+    logger.info("Fetching quotes from the page...")
     quotes = []
 
     for quote in soup.find_all('div', class_='quote'):
@@ -33,11 +37,18 @@ def fetch_quotes(soup, base_url):
             'tags': tags
         })
 
+    logger.info(f"Extracted {len(quotes)} quotes")
     return quotes
 
 
 def fetch_author_details(author_url):
-    response = requests.get(author_url)
+    try:
+        response = requests.get(author_url)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        logger.error(f"Error fetching author details from {author_url}: {e}")
+        return None
+
     soup = BeautifulSoup(response.text, 'lxml')
     fullname = extract_text(
         soup.find('h3', class_='author-title'), 'Unknown Author'
@@ -54,6 +65,8 @@ def fetch_author_details(author_url):
         soup.find('div', class_='author-description'), 'No bio available'
     )
 
+    logger.info(f"Fetched details for author {fullname}")
+
     return {
         "fullname": fullname,
         "birth_date": birth_date,
@@ -63,7 +76,13 @@ def fetch_author_details(author_url):
 
 
 def scrape_page_data(base_url, page_url, fetched_authors):
-    response = requests.get(page_url)
+    try:
+        response = requests.get(page_url)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        logger.error(f"Error fetching page {page_url}: {e}")
+        return [], []
+
     soup = BeautifulSoup(response.text, 'lxml')
 
     quotes = fetch_quotes(soup, base_url)
@@ -81,6 +100,7 @@ def scrape_page_data(base_url, page_url, fetched_authors):
 
 
 def scrape_all_data(base_url):
+    logger.info(f"Starting scraping from {base_url}")
     all_quotes = []
     all_authors_details = []
     fetched_authors = set()
@@ -95,6 +115,11 @@ def scrape_all_data(base_url):
         next_url = get_next_page_url(
             current_url, base_url, ['li.next a'], 'href'
         )
-        current_url = next_url
+        if next_url:
+            logger.info(f"Fetching next page: {next_url}")
+            current_url = next_url
+        else:
+            logger.info("No more pages to fetch.")
+            break
 
     return all_quotes, all_authors_details
